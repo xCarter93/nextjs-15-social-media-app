1218d788fe4652bbba581fa5a3339c4b232dac46
Patrick Carter <xCarter93@gmail.com>
2024-08-08 12:45:29 -0400

    set up backend auth logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | bugbook",
-    default: "bugbook",
+    template: "%s | woofbook",
+    default: "woofbook",
   },
   description: "The social media app for powernerds",
 };
