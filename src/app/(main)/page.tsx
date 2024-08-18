2156a47e6ffee4107019474d9613810cfd2d743c
Patrick Carter <xCarter93@gmail.com>
2024-08-18 14:32:30 -0400

    created post editor component and wired up the create post method

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,12 @@
+import PostEditor from "@/components/posts/editor/PostEditor";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Front Page</main>;
+  return (
+    <main className="flex w-full min-w-0 gap-5">
+      <div className="w-full min-w-0 space-y-5">
+        <PostEditor />
+      </div>
+    </main>
+  );
 }
